e8f339d54cbee190c27a2e63093b9b7766b58c4e
Eric Lamb <eric@mithra62.com>
2025-01-09 19:23:13 -0700

    moves backup default view

--- system/ee/ExpressionEngine/View/utilities/backups/index.php
+++ system/ee/ExpressionEngine/View/utilities/backups/index.php
@@ -1,18 +1,18 @@
 <?php $this->extend('_templates/default-nav', [], 'outer_box'); ?>
+
 <div class="box add-mrg-bottom">
     <?php echo ee('CP/Alert')->getAllInlines(); ?>
 </div>
 <div class="box table-list-wrap">
     <fieldset class="tbl-search right">
-        <a class="btn tn action" target="_blank" href="<?php echo ee('CP/URL')->make('utilities/db-backup'); ?>"><?php echo lang('bm.create.new_backup'); ?></a>
+        <a class="btn tn action" target="_blank" href="<?php echo ee('CP/URL')->make('utilities/db-backup'); ?>"><?php echo lang('backup_database'); ?></a>
     </fieldset>
     <?php echo form_open($base_url, 'class="tbl-ctrls"'); ?>
-    <h1><?php echo lang('bm.backup_manager_header'); ?></h1>
+    <h1><?php echo lang('backups'); ?></h1>
     <div class="app-notice-wrap">
         <?php echo ee('CP/Alert')->get('items-table'); ?>
     </div>
 
     <?php $this->embed('ee:_shared/table', $table); ?>
-    <?php echo $pagination; ?>
     <?php echo form_close(); ?>
 </div>
